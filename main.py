9fff91a61feaeba3ae89d1a211a6f1b312798df4
gordon-coder <gordonpeng27@gmail.com>
2026-01-30 21:28:52 +0800

    added sample emmisions from uni startup

---
+++ main.py
@@ -0,0 +1,20 @@
+from netCarbonEmissions import *
+from csv import reader
+
+#region <Load product data from CSV>
+products = []
+with open("./emissionData.csv", 'r') as file:
+    csvReader = reader(file)
+    next(csvReader)  # Skip header
+    for row in csvReader:
+        name = row[0]
+        amount = float(row[1])
+        averageAvoidedEmissions = float(row[2])
+        averageStorageEmissions = float(row[3])
+        product = Product(name, amount, averageAvoidedEmissions, averageStorageEmissions)
+        products.append(product)
+
+#endregion
+
+netCarbonReduction = calculateNetCarbonEmmisions(products, totalTransportDistance=50*20, averageTransportEmissionsPerKm=0.2)
+print(f"Net Carbon Reduction: {netCarbonReduction} kg CO2")
